b633216ea70cf4331372f783c6699569cf547774
Meshack Konadu <mesh@icynets.com>
2020-01-27 01:14:43 +0200

    Second Commit

--- app/src/main/java/com/icynets/reminder2020/MainActivity.kt
+++ app/src/main/java/com/icynets/reminder2020/MainActivity.kt
@@ -12,6 +12,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var fabOpened = false
+
+        fab.setOnClickListener{
+            if (!fabOpened){
+
+                fabOpened = true
+                fab_map.animate().translationY(-resources.getDimension(R.dimen.standard_66))
+                fab_time.animate().translationY(-resources.getDimension(R.dimen.standard_116))
+
+            }else{
+
+                fabOpened = false
+                fab_map.animate().translationY(0f)
+                fab_time.animate().translationY(0f)
+            }
+        }
+
         fab_time.setOnClickListener{
             val intent = Intent(applicationContext, TimeActivity::class.java)
             startActivity(intent)
