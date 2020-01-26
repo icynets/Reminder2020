68b2dba074c41a1a3f09b420ddaae1562bb04306
Meshack Konadu <mesh@icynets.com>
2020-01-27 00:53:41 +0200

    Initial commit

---
+++ app/src/main/java/com/icynets/reminder2020/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.icynets.reminder2020
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fab_time.setOnClickListener{
+            val intent = Intent(applicationContext, TimeActivity::class.java)
+            startActivity(intent)
+        }
+
+        fab_map.setOnClickListener{
+            val intent = Intent(applicationContext, MapActivity::class.java)
+
+            startActivity(intent)
+        }
+
+    }
+}
