da36e47f6bde8d918980ac571cfa68ac146af143
icynets <mesh@icynets.com>
2020-01-28 09:42:29 +0200

    List View Item with Database

---
+++ app/src/main/java/com/icynets/reminder2020/AppDatabase.kt
@@ -0,0 +1,9 @@
+package com.icynets.reminder2020
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Reminder::class], version = 1)
+abstract class AppDatabase : RoomDatabase(){
+    abstract fun reminderDao() : ReminderDao
+}
